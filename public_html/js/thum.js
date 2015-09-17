a548d5279bd94a567f03b9a4e350b6138a2998eb
Andrey <andxbes@gmail.com>
2015-09-17 19:20:06 +0300

    add contacts

--- public_html/js/thum.js
+++ public_html/js/thum.js
@@ -9,6 +9,13 @@ $( document ).ready(function() {
             $(this).find('.caption').slideUp(250); //.fadeOut(205)
         }
     ); 
+       
+    
+     $.plot("#chart1", [{data: 75, color: '#42b8dd'}, {data: 25, color: '#fff'}], {series: {pie: {show: true, innerRadius: 0.87}}});
+     $.plot("#chart2", [{data: 25, color: '#42b8dd'}, {data: 75, color: '#fff'}], {series: {pie: {show: true, innerRadius: 0.87}}});
+     $.plot("#chart3", [{data: 75, color: '#42b8dd'}, {data: 25, color: '#fff'}], {series: {pie: {show: true, innerRadius: 0.87}}});
+     $.plot("#chart4", [{data: 50, color: '#42b8dd'}, {data: 50, color: '#fff'}], {series: {pie: {show: true, innerRadius: 0.87}}});
+
 });
 
 
